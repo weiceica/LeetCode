b076baa5f811eea3c38d7961e1fa272020c2df74
weiceica <weiceica@umich.edu>
2023-02-20 00:17:30 -0500

    2/23/2023

---
+++ interview/3-longest-string-without-repetition.cpp
@@ -0,0 +1,21 @@
+#include <unordered_set>
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        // basecase
+        if(s.length() == 1) return 1;
+        unordered_set <char> c;
+        int l = 0;
+        int result = 0;
+        c.insert(s[0]);
+        for(int i = 1; i < s.length(); ++i){
+            while(c.find(s[i]) != c.end()){
+                c.erase(s[l]);
+                l++;
+            }
+            c.insert(s[i]);
+            result = max(result, i - l + 1);
+        }
+        return result;
+    }
+};
