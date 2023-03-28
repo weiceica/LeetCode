247b7cdaa09b1f03dc8e5e5cb41001197a83c542
weiceica <weiceica@umich.edu>
2023-03-28 04:03:34 -0400

    transitioning to python...

---
+++ neetcode/hashing/hashing#6.py
@@ -0,0 +1,13 @@
+
+from typing import List
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = []
+        res.append(1)
+        for i in range(1,len(nums)):
+            res.append(res[i-1] * nums[i-1])
+        right = 1
+        for i in range(len(nums)-1,-1,-1):
+            res[i] *= right
+            right *= nums[i]
+        return res
