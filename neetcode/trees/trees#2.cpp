4a9d022e7e28eaf1372919329d78e1b753abea98
weiceica <weiceica@umich.edu>
2023-05-29 17:49:22 -0400

    5/29/2023

---
+++ neetcode/trees/trees#2.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+using namespace std;
+
+struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        int i = 1;
+        if(!root) return 0;
+        i += max(maxDepth(root->left), maxDepth(root->right));
+        return i;
+    }   
+};
