2c11b68bc5b945be36f996feb00c48a78263af4c
weiceica <weiceica@umich.edu>
2023-05-29 23:51:49 -0400

    5/30/2023 iterative inorder traversal

---
+++ neetcode/trees/trees#8.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <stack>
+using namespace std;
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        stack<TreeNode*> stack;
+        TreeNode* current = root;
+        vector<int> inorder;
+        while (current || !stack.empty()) {
+            while (current) {
+                stack.push(current);
+                current = current->left;
+            }   
+            current = stack.top();
+            inorder.push_back(current->val);
+            stack.pop();
+            current = current->right;
+        }
+        return inorder[k-1];
+    }
+};
