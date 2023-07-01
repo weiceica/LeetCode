0b8170ee8cb49739692aa95bb7a8a4ad85e9b5fa
weiceica <weiceica@umich.edu>
2023-07-01 15:36:33 -0400

    7/3/2023

--- neetcode/backtracking/backtracking#1.cpp
+++ neetcode/backtracking/backtracking#1.cpp
@@ -2,25 +2,23 @@
 using namespace std;
 class Solution {
 public:
-// nums = [1,2,3]
-// retval: [[],[1],[1,2],[1,2,3]]
-// currentSet: [1,2]
-// i = 3
+/*
+nums = [1,2,3]
+[[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
 
-    void dfs(vector<int>&nums, int index, vector<int>& currentSet, vector<vector<int>>& retval){
-        retval.push_back(currentSet);
-
-        for(int i = index; i < nums.size(); ++i){
-            currentSet.push_back(nums[i]);
-            dfs(nums, i+1, currentSet, retval);
-            currentSet.pop_back();
-        }
-    }
-
+*/
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> retval;
-        vector<int> currentSet;
-        dfs(nums, 0, currentSet, retval);
-        return retval;
+        vector<vector<int>> subsets;
+        vector<int> curSet;
+        subsets.push_back(curSet);
+        for (int num : nums) {
+            int currentSubsetCount = subsets.size();
+            for (int i = 0; i < currentSubsetCount; ++i) {
+                vector<int> newSubset = subsets[i];
+                newSubset.push_back(num);
+                subsets.push_back(newSubset);
+            }
+        }
+        return subsets;
     }
 };
